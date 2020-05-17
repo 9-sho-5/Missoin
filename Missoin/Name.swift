f4d71f7b3a5a2105a8b8186a8455ab5407062a59
Hosho Kusunose <hosho.kusu@icloud.com>
2020-05-17 14:51:28 +0900

    once finished

---
+++ Missoin/Name.swift
@@ -0,0 +1,21 @@
+//
+//  Name.swift
+//  Missoin
+//
+//  Created by Kusunose Hosho on 2020/05/17.
+//  Copyright © 2020 Kusunose Hosho. All rights reserved.
+//
+
+import Foundation
+
+class Name {
+    var name: String!
+    
+    var number: String!
+    
+    init(number: String, name: String) {
+        
+        self.number = number
+        self.name = name
+    }
+}
--- Missoin/ViewController.swift
+++ Missoin/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  Missoin
+//  MentorBook
 //
-//  Created by Kusunose Hosho on 2020/05/17.
+//  Created by Kusunose Hosho on 2020/05/13.
 //  Copyright © 2020 Kusunose Hosho. All rights reserved.
 //
 
@@ -10,11 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var nameArray: [Name] = []
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var numberLabel: UILabel!
+    
+    //表示する番号
+    var index: Int = 0
+    
+    func setUI() {
+        numberLabel.text = nameArray[index].number
+        nameLabel.text = nameArray[index].name
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        nameArray.append(Name(number: "１番目", name: "A"))
+        nameArray.append(Name(number: "２番目",name: "B"))
+        nameArray.append(Name(number: "３番目",name: "C"))
+        nameArray.append(Name(number: "4番目",name: "D"))
+        nameArray.append(Name(number: "5番目",name: "C"))
+        
+        
+        setUI()
     }
 
+    //巡回するようにindexの値の変更
+    @IBAction func pre() {
+        index = index - 1
+        if index < 0 {
+            index = 4
+        }
+        setUI()
+    }
+    
+    @IBAction func next() {
+        index = index + 1
+        if index == 5 {
+            index = 0
+        }
+        setUI()
+    }
+    
+    @IBAction func one() {
+        nameLabel.text = nameArray[0].name
+        numberLabel.text = nameArray[0].number
+    }
 
+    @IBAction func two() {
+        nameLabel.text = nameArray[1].name
+        numberLabel.text = nameArray[1].number
+    }
+    
+    @IBAction func three() {
+        nameLabel.text = nameArray[2].name
+        numberLabel.text = nameArray[2].number
+    }
+    
 }
-
