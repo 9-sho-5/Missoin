9336afeb06badb4975fad56ceb3bceeb45fb769a
Hosho Kusunose <hosho.kusu@icloud.com>
2020-05-17 14:10:00 +0900

    Initial Commit

---
+++ Missoin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Missoin
+//
+//  Created by Kusunose Hosho on 2020/05/17.
+//  Copyright © 2020 Kusunose Hosho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
