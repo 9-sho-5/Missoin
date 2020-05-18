860b7155abd1d16a20c11a17e9ecd220d1e26320
Hosho Kusunose <hosho.kusu@icloud.com>
2020-05-18 22:35:23 +0900

    modified one two three

--- Missoin/ViewController.swift
+++ Missoin/ViewController.swift
@@ -54,24 +54,21 @@ class ViewController: UIViewController {
     }
     
     @IBAction func one() {
-        nameLabel.text = nameArray[0].name
-        numberLabel.text = nameArray[0].number
-        
         index = 0
+        
+        setUI()
     }
 
     @IBAction func two() {
-        nameLabel.text = nameArray[1].name
-        numberLabel.text = nameArray[1].number
-        
         index = 1
+        
+        setUI()
     }
     
     @IBAction func three() {
-        nameLabel.text = nameArray[2].name
-        numberLabel.text = nameArray[2].number
-        
         index = 2
+        
+        setUI()
     }
     
 }
